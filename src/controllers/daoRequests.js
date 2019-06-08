9fd6030282f8f855c60fee87b5be7bda0f45aec0
André Carlos <andre-carlos@live.com>
2019-06-08 13:04:04 -0300

    Atualizando layout

--- src/controllers/daoRequests.js
+++ src/controllers/daoRequests.js
@@ -46,7 +46,7 @@ class mercadobitcoinApi{
         let date = convertDate(data.ticker.date)
         let ultimoValor = float(data.ticker.last).toFixed(2)
         let maxValor24 = float(data.ticker.high).toFixed(2)
-        let textOperation = `$${this.coin}===<b>${this.discoverName()} \ ${date}</b>===$${this.coin}\nMaior valor nas ultimas 24h: R$${maxValor24}\n<b>Valor atual:</b> R$<b>${ultimoValor}</b>\n\n<b>Volte ao</b> /menu `
+        let textOperation = `$${this.coin} <b>${this.discoverName()} / ${date}</b>\nMaior valor nas ultimas 24h: R$${maxValor24}\n<b>Valor atual:</b> R$<b>${ultimoValor}</b>\n\n<b>Volte ao</b> /menu `
         return textOperation
     }
 
