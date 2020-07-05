b9a91f3b526d8635e1189ba0608bfb54afef1b9b
André Carlos Jr <andre-carlos@live.com>
2020-07-05 13:15:04 -0300

    change port

--- index.js
+++ index.js
@@ -26,7 +26,5 @@ app.get("/commands", (req, res) => {
 let port = process.env.PORT || 3000;
 
 let server = app.listen(port, "0.0.0.0", function () {
-  console.log(
-    `Funcionando ${server.address().address}:${server.address().port}`
-  );
+  console.log(`Funcionando ${server.address().address}:${process.env.PORT}`);
 });
